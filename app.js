10c5fc8ce87c17d73484b627e4490f9e24fb1a5c
Nguyen Huu Nghia <nhnghiacntt@gmail.com>
2023-12-21 15:59:30 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
+const contactRouter = require("./app/routes/contact.route");
+
 
 app.use(cors());
 app.use(express.json());
-
-
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
-});
+app.use("/api/contacts", contactRouter);
 
 
 module.exports = app;
